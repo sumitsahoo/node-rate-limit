41c9e5a11b717843a9547acf3d04f6ea829d6290
Sumit Sahoo <sumitsahoo1988@gmail.com>
2025-06-03 17:23:55 +0530

    feat: update project configuration and add biome for linting and formatting

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express();
 const port = 3000;
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
-  message: "Go away you spammer! 😡",
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 5, // limit each IP to 5 requests per windowMs
+	message: "Go away you spammer! 😡",
 });
 
 const speedLimiter = slowDown({
-  windowMs: 15 * 60 * 1000, // 15 minutes   
-  delayAfter: 1, // allow 1 request per 15 minutes
-  delayMs: () => 2000,
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	delayAfter: 1, // allow 1 request per 15 minutes
+	delayMs: () => 2000,
 });
 
 app.use(speedLimiter);
@@ -22,9 +22,9 @@ app.use(limiter);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
